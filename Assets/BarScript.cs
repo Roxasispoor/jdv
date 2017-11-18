3971bddc177b0bc5241ef5f84e4d6597c6193fd2
nicolasrygiel <nicolas.rygiel@gmail.com>
2017-11-18 15:51:48 +0100

    première version hud

---
+++ Assets/BarScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class BarScript : MonoBehaviour {
+	private float fillAmount;
+	public float Value 
+	{
+		set {
+			fillAmount = value;
+		}
+	}
+
+	[SerializeField]
+	private Image content;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		HandleBar ();
+	}
+	private void HandleBar()
+	{
+		if (fillAmount != content.fillAmount) 
+		{
+			content.fillAmount = fillAmount;
+		}
+	}
+}
