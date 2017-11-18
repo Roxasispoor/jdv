c74f1ba94a338ed879ea64bb261f474c84bd984b
Manzano alban <manzanoalban@gmail.com>
2017-11-18 04:20:37 +0100

    First commit

---
+++ Assets/Actor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour {
+    public enum CharacterEmotion
+    { NORMAL, SPECIAL }
+    CharacterEmotion characterEmo;
+    public Sprite[] sprites;
+    SpriteRenderer spriteRender;
+    private int sens;//0gauche 1droite 2gauche coupé 3droitecoupé
+    private int positionNum; // 0 tout à gauche 3 tout à droite
+    public CharacterEmotion CharacterEmo
+    {
+        get
+        {
+            return characterEmo;
+        }
+
+        set
+        {
+            characterEmo = value;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start() {
+        this.CharacterEmo = CharacterEmotion.NORMAL;
+        this.spriteRender = GetComponent<SpriteRenderer>();
+    }
+    public void ChangeState(string name)
+    {
+        StartCoroutine(name + "State");
+    }
+    IEnumerator NORMALState()
+        { spriteRender.sprite = sprites[0];
+        yield return null;
+
+        }
+    IEnumerator SPECIALState()
+    {
+        spriteRender.sprite = sprites[0];
+        yield return null;
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public GameObject[] prefabsActors;
+    public List<GameObject> listeActors = new List<GameObject>(); 
+
+    // Use this for initialization
+    void Start () {
+        for(int i=0;i<prefabsActors.Length;i++)
+        {
+            GameObject newActor = Instantiate(prefabsActors[i]);
+            newActor.SetActive(false);
+            newActor.name = prefabsActors[i].name;
+            listeActors.Add(newActor);
+           
+            
+            //bool activeSelf
+        }
+		
+	}
+
+    public void instanciateActors()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ChangeState()
+    { }
+}
